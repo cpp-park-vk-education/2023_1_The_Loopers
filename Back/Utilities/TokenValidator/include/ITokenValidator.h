d9818d597f73486d0b1a74543ba8407094a331a4
Novak Alexander <sasha_7tdd7@mail.ru>
2023-05-04 03:41:30 +0300

    Add network adapter

--- Back/Utilities/TokenValidator/include/ITokenValidator.h
+++ Back/Utilities/TokenValidator/include/ITokenValidator.h
@@ -8,8 +8,9 @@ class ITokenValidator {
   public:
     virtual ~ITokenValidator() = default;
 
-    virtual std::chrono::time_point<std::chrono::system_clock> ValidateToken(std::string token,
-                                                                             std::string publicKey);
+    virtual std::chrono::time_point<std::chrono::system_clock> ValidateToken(
+            std::string token, std::string publicKey) {
+    }
 };
 
 #endif  // _ITOKENVALIDATOR_H_
