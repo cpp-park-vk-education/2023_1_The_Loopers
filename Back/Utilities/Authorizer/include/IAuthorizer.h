850cd2a0ca883cd8263cf09ae6e343f2ea4e5d67
Alexander Novak <sasha_7tdd7@mail.ru>
2023-05-04 11:42:07 +0300

    Add server network

---
+++ Back/Utilities/Authorizer/include/IAuthorizer.h
@@ -0,0 +1,20 @@
+#ifndef _IAUTHORIZER_H_
+#define _IAUTHORIZER_H_
+
+#include <memory>
+#include <string>
+
+#include "ITokenValidator.h"
+
+class IAuthorizer {
+  public:
+    virtual ~IAuthorizer() = default;
+
+    virtual void SetTokenValidator(ITokenValidator*) {
+    }
+
+    virtual bool IsAuthenticated(const std::string&) {
+    }
+};
+
+#endif  // _IAUTHORIZER_H_
