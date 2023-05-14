bcca26f681847217a638a9d1360de15aca5550ca
Gallaann <reindzer26rus@gmail.com>
2023-05-15 01:01:12 +0300

    REF added missing &

--- frontend/auth/include/Encrypter.hpp
+++ frontend/auth/include/Encrypter.hpp
@@ -5,7 +5,7 @@
 class Encrypter{
 public:
     void setEncryptingKey(std::string key);
-    std::string encryptString(const std::string stringToEncrypt);
+    std::string encryptString(const std::string& stringToEncrypt);
 
 private:
     std::string m_encryptingKey;
