a1f4ccbbe115d9f2a1bd26abc57e36ec24814b30
Gallaann <reindzer26rus@gmail.com>
2023-05-15 01:03:12 +0300

    FIX order public - protected - private

--- frontend/draw/include/DrawModel.h
+++ frontend/draw/include/DrawModel.h
@@ -1,22 +1,25 @@
 #pragma once
 
+#include "IObject.h"
+
 #include <string>
 #include <vector>
 
 struct Point {
-    size_t xPosition;
-    size_t yPosition;
+    std::size_t xPosition;
+    std::size_t yPosition;
 };
 
 class DrawModel {
 public:
     void deserialize(std::string);
 
-private:
-    std::vector<ObjectWithAttributes*> m_objects;
 protected:
     void addObject(size_t, size_t, std::vector<Point>);
     void addObject(size_t, Point, size_t, size_t);
     void addObject(size_t, std::string, Point, size_t);
     std::string serialize();
+
+private:
+    std::vector<ObjectWithAttributes*> m_objects;
 };
