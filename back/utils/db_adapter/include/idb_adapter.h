1aba3a757f9e640f9c6566d315d47b55879686c4
kingjoram <alex.v.danilchenko@yandex.ru>
2023-05-15 19:39:36 +0300

    INKLB-007 REF:database:db_adapter:refactor idb_adapter.h

--- back/utils/db_adapter/include/idb_adapter.h
+++ back/utils/db_adapter/include/idb_adapter.h
@@ -1,23 +1,26 @@
-#ifndef _IDBADAPTER_H_
-#define _IDBADAPTER_H_
+#ifndef _BACK_UTILS_DB_ADAPTER_IDBADAPTER_H_
+#define _BACK_UTILS_DB_ADAPTER_IDBADAPTER_H_
 
-#include <memory>
 #include <string>
 
 class IDBAdapter {
   public:
     virtual ~IDBAdapter() = default;
 
-    virtual void Insert(const std::string&) {
-    }
-    virtual void Update(const std::string&) {
-    }
-    virtual std::string Select(const std::string&) {
-    }
-    virtual void Delete(const std::string&) {
-    }
-    virtual void Connect(const std::string&) {
-    }
+    virtual bool Insert(const std::string& tableName, const std::string& login,
+                        const std::string& fileName, const std::string& filePath) = 0;
+
+    virtual bool Update(const std::string& tableName, const std::string& login,
+                        const std::string& fileName) = 0;
+
+    virtual std::string Select(const std::string& tableName, const std::string& login,
+                               const std::string fileName) = 0;
+
+    virtual bool Delete(const std::string& tableName, const std::string& login,
+                        const std::string& fileName) = 0;
+
+    virtual bool Connect() = 0;
+    virtual bool Disconnect() = 0;
 };
 
-#endif  // _IDBADAPTER_H_
+#endif  // _BACK_UTILS_DB_ADAPTER_IDBADAPTER_H_
