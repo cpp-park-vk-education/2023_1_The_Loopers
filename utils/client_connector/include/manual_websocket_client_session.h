f7448da3bed598d33b5ea215fed04a7ae097a7fe
Alexander Novak <sasha_7tdd7@mail.ru>
2023-05-15 04:44:40 +0300

    INKLB-007 after merge

--- utils/client_connector/include/manual_websocket_client_session.h
+++ utils/client_connector/include/manual_websocket_client_session.h
@@ -1,23 +1,29 @@
 #ifndef _MANUAL_WEBSOCKET_CLIENT_SESSION_H_
 #define _MANUAL_WEBSOCKET_CLIENT_SESSION_H_
 
+#include "global_websocket_client_session.h"
+#include "iclient_session.h"
+
+#include <boost/asio.hpp>
+#include <boost/beast.hpp>
+#include <boost/system/error_code.hpp>
 #include <deque>
 #include <functional>
 #include <memory>
 #include <string>
 
-#include "global_websocket_client_session.h"
-#include "iclient_session.h"
-
-namespace inklink_client_session
+namespace inklink::client_connector
 {
+namespace net = boost::asio;
+namespace beast = boost::beast;
+
 template <Do_ConnectTypeErrorCode_Concept DoOnConnectType = Fun_ConnectTypeErrorCodeSession,
           Do_StringErrorCode_Concept DoOnRead = Fun_StringErrorCodeSession,
-          Do_ErrorCode_Concept DoOnWrite = Fun_ErrorCode,
-          Do_ErrorCode_Concept DoOnClose = Fun_ErrorCode>
+          Do_ErrorCode_Concept DoOnWrite = Fun_ErrorCode, Do_ErrorCode_Concept DoOnClose = Fun_ErrorCode>
 class ManualWebsocketClientSession
         : public IClientSession,
-          public std::enable_shared_from_this<ManualWebsocketClientSession>
+          public std::enable_shared_from_this<
+                  ManualWebsocketClientSession<DoOnConnectType, DoOnRead, DoOnWrite, DoOnClose>>
 {
 public:
     ManualWebsocketClientSession() = delete;
@@ -26,8 +32,7 @@ public:
             boost::asio::io_context&, const std::string& address, unsigned short port,
             DoOnConnectType = [](ConnectType ct, boost::system::error_code) {},
             DoOnRead = [](const std::string&, boost::system::error_code) {},
-            DoOnWrite = [](boost::system::error_code) {},
-            DoOnClose = [](boost::system::error_code) {});
+            DoOnWrite = [](boost::system::error_code) {}, DoOnClose = [](boost::system::error_code) {});
 
     ManualWebsocketClientSession(const ManualWebsocketClientSession&) = delete;
     ManualWebsocketClientSession(ManualWebsocketClientSession&&) = delete;
@@ -42,12 +47,11 @@ public:
     void Close() override;
 
 private:
-    namespace net = boost::asio;
-    namespace beast = boost::beast;
     using error_code = boost::system::error_code;
 
-    void OnConnect(error_code ec);
+    void DoRead();
 
+    void OnConnect(error_code ec);
     void OnHandshake(error_code ec);
     void OnWrite(error_code ec, std::size_t bytes_transferred);
     void OnRead(error_code ec, std::size_t bytes_transferred);
@@ -56,18 +60,17 @@ private:
     boost::asio::ip::tcp::socket m_socket;
     boost::asio::deadline_timer m_timer;
 
-    const std::string& m_host;
+    std::string m_host;
     beast::websocket::stream<beast::tcp_stream> m_ws;
 
     beast::flat_buffer m_buffer;
     std::deque<std::shared_ptr<std::string const>> m_queue;
-    boost::asio::buffer m_bufferSending;
 
     DoOnConnectType m_doOnConnectType;
     DoOnRead m_doOnRead;
     DoOnWrite m_doOnWrite;
     DoOnClose m_doOnClose;
 };
-}  // namespace inklink_client_session
+} // namespace inklink::client_connector
 
-#endif  // _MANUAL_WEBSOCKET_CLIENT_SESSION_H_
+#endif // _MANUAL_WEBSOCKET_CLIENT_SESSION_H_
--- utils/client_connector/src/manual_websocket_client_session.cpp
+++ utils/client_connector/src/manual_websocket_client_session.cpp
@@ -1,8 +1,8 @@
-#include <boost/posix_time.h>
-
 #include "manual_client_session.h"
 
-namespace inklink_client_session
+#include <boost/posix_time.h>
+
+namespace inklink::client_connector
 {
 namespace net = boost::asio;
 namespace beast = boost::beast;
@@ -11,11 +11,10 @@ using error_code = boost::system::error_code;
 template <Do_ConnectTypeErrorCode_Concept DoOnConnectType, Do_StringErrorCode_Concept DoOnRead,
           Do_ErrorCode_Concept DoOnWrite, Do_ErrorCode_Concept DoOnClose>
 WebsocketClientSession<DoOnConnectType, DoOnRead, DoOnWrite, DoOnClose>::WebsocketClientSession(
-        net::io_context& ioc, const std::string& address, unsigned short port,
-        DoOnConnectType doOnConnectType, DoOnRead doOnRead, DoOnWrite doOnWrite,
-        DoOnClose doOnClose)
-        : m_timer{ioc}, m_doOnConnectType{doOnConnectType}, m_doOnRead{doOnRead},
-          m_doOnWrite{doOnWrite}, m_doOnClose{doOnClose}
+        net::io_context& ioc, const std::string& address, unsigned short port, DoOnConnectType doOnConnectType,
+        DoOnRead doOnRead, DoOnWrite doOnWrite, DoOnClose doOnClose)
+        : m_timer{ioc}, m_doOnConnectType{doOnConnectType}, m_doOnRead{doOnRead}, m_doOnWrite{doOnWrite},
+          m_doOnClose{doOnClose}
 {
     net::ip::tcp::endpoint localEndpoint(boost::asio::ip::address::from_string(address), port);
     // do not realy need strand here, and I'm not sure can be socket constructed using strand
@@ -24,8 +23,8 @@ WebsocketClientSession<DoOnConnectType, DoOnRead, DoOnWrite, DoOnClose>::Websock
 
 template <Do_ConnectTypeErrorCode_Concept DoOnConnectType, Do_StringErrorCode_Concept DoOnRead,
           Do_ErrorCode_Concept DoOnWrite, Do_ErrorCode_Concept DoOnClose>
-void WebsocketClientSession<DoOnConnectType, DoOnRead, DoOnWrite, DoOnClose>::RunAsync(
-        const std::string& host, unsigned short port)
+void WebsocketClientSession<DoOnConnectType, DoOnRead, DoOnWrite, DoOnClose>::RunAsync(const std::string& host,
+                                                                                       unsigned short port)
 {
     m_host = host;
     m_host += ':' + std::to_string(port);
@@ -52,8 +51,7 @@ void WebsocketClientSession<DoOnConnectType, DoOnRead, DoOnWrite, DoOnClose>::Ru
 
 template <Do_ConnectTypeErrorCode_Concept DoOnConnectType, Do_StringErrorCode_Concept DoOnRead,
           Do_ErrorCode_Concept DoOnWrite, Do_ErrorCode_Concept DoOnClose>
-void WebsocketClientSession<DoOnConnectType, DoOnRead, DoOnWrite, DoOnClose>::Send(
-        const std::string& msgBody)
+void WebsocketClientSession<DoOnConnectType, DoOnRead, DoOnWrite, DoOnClose>::Send(const std::string& msgBody)
 {
     auto ss = std::make_shared<std::string const>(message);
     // Always add to queue
@@ -64,10 +62,8 @@ void WebsocketClientSession<DoOnConnectType, DoOnRead, DoOnWrite, DoOnClose>::Se
         return;
 
     // We are not currently writing, so send this immediately
-    m_bufferSending = net::buffer(*m_queue.front());
-
-    m_ws.async_write(m_bufferSending, beast::bind_front_handler(&WebsocketClientSession::OnWrite,
-                                                                shared_from_this()));
+    m_ws.async_write(net::buffer(*m_queue.front()),
+                     beast::bind_front_handler(&WebsocketClientSession::OnWrite, this->shared_from_this()));
 }
 
 template <Do_ConnectTypeErrorCode_Concept DoOnConnectType, Do_StringErrorCode_Concept DoOnRead,
@@ -76,20 +72,28 @@ void WebsocketClientSession<DoOnConnectType, DoOnRead, DoOnWrite, DoOnClose>::Cl
 {
     // TODO (a.novak) stop every read etc.
 
-    m_ws.async_close(
-            websocket::close_code::normal,
-            beast::bind_front_handler(&WebsocketClientSession::OnClose, shared_from_this()));
+    m_ws.async_close(websocket::close_code::normal,
+                     beast::bind_front_handler(&WebsocketClientSession::OnClose, this->shared_from_this()));
 }
 
 template <Do_ConnectTypeErrorCode_Concept DoOnConnectType, Do_StringErrorCode_Concept DoOnRead,
           Do_ErrorCode_Concept DoOnWrite, Do_ErrorCode_Concept DoOnClose>
-void WebsocketClientSession<DoOnConnectType, DoOnRead, DoOnWrite, DoOnClose>::OnConnect(
-        error_code ec)
+void WebsocketClientSession<DoOnConnectType, DoOnRead, DoOnWrite, DoOnClose>::DoRead()
+{
+    // Read a message into our buffer
+    m_ws.async_read(m_buffer, beast::bind_front_handler(&WebsocketClientSession::OnRead, this->shared_from_this()));
+}
+
+template <Do_ConnectTypeErrorCode_Concept DoOnConnectType, Do_StringErrorCode_Concept DoOnRead,
+          Do_ErrorCode_Concept DoOnWrite, Do_ErrorCode_Concept DoOnClose>
+void WebsocketClientSession<DoOnConnectType, DoOnRead, DoOnWrite, DoOnClose>::OnConnect(error_code ec)
 {
     m_timer->cancel();
     m_doOnConnectType(ConnectType::kConnect, ec);
     if (ec)
+    {
         return;
+    }
 
     m_ws = websocket::stream<net::ip::tcp::socket>(std::move(m_socket));
 
@@ -103,40 +107,36 @@ void WebsocketClientSession<DoOnConnectType, DoOnRead, DoOnWrite, DoOnClose>::On
     // Set a decorator to change the User-Agent of the handshake
     m_ws.set_option(websocket::stream_base::decorator(
             [](websocket::request_type& req)
-            {
-                req.set(http::field::user_agent,
-                        std::string(BOOST_BEAST_VERSION_STRING) + " inklink-client");
-            }));
+            { req.set(http::field::user_agent, std::string(BOOST_BEAST_VERSION_STRING) + " inklink-client"); }));
 
     // Perform the websocket handshake
-    m_ws.async_handshake(
-            m_host, "/",
-            beast::bind_front_handler(&WebsocketClientSession::OnHandshake, shared_from_this()));
+    m_ws.async_handshake(m_host, "/",
+                         beast::bind_front_handler(&WebsocketClientSession::OnHandshake, this->shared_from_this()));
 }
 
 template <Do_ConnectTypeErrorCode_Concept DoOnConnectType, Do_StringErrorCode_Concept DoOnRead,
           Do_ErrorCode_Concept DoOnWrite, Do_ErrorCode_Concept DoOnClose>
-void WebsocketClientSession<DoOnConnectType, DoOnRead, DoOnWrite, DoOnClose>::OnHandshake(
-        error_code ec)
+void WebsocketClientSession<DoOnConnectType, DoOnRead, DoOnWrite, DoOnClose>::OnHandshake(error_code ec)
 {
     m_doOnConnectType(ConnectType::kHandshake, ec);
     if (ec)
+    {
         return;
+    }
 
-    // Read a message into our buffer
-    m_ws.async_read(m_buffer,
-                    beast::bind_front_handler(&WebsocketClientSession::OnRead, shared_from_this()));
+    DoRead();
 }
 
 template <Do_ConnectTypeErrorCode_Concept DoOnConnectType, Do_StringErrorCode_Concept DoOnRead,
           Do_ErrorCode_Concept DoOnWrite, Do_ErrorCode_Concept DoOnClose>
-void WebsocketClientSession<DoOnConnectType, DoOnRead, DoOnWrite, DoOnClose>::OnWrite(error_code ec,
-                                                                                      std::size_t)
+void WebsocketClientSession<DoOnConnectType, DoOnRead, DoOnWrite, DoOnClose>::OnWrite(error_code ec, std::size_t)
 {
     m_doOnWrite(ec);
 
     if (ec)
+    {
         return;
+    }
 
     // Remove the string from the queue
     m_queue.pop_front();
@@ -144,21 +144,20 @@ void WebsocketClientSession<DoOnConnectType, DoOnRead, DoOnWrite, DoOnClose>::On
     // Send the next message if any
     if (!m_queue.empty())
     {
-        m_bufferSending = net::buffer(*m_queue.front());
-        m_ws.async_write(
-                m_bufferSending,
-                beast::bind_front_handler(&WebsocketClientSession::OnWrite, shared_from_this()));
+        m_ws.async_write(net::buffer(*m_queue.front()),
+                         beast::bind_front_handler(&WebsocketClientSession::OnWrite, this->shared_from_this()));
     }
 }
 
 template <Do_ConnectTypeErrorCode_Concept DoOnConnectType, Do_StringErrorCode_Concept DoOnRead,
           Do_ErrorCode_Concept DoOnWrite, Do_ErrorCode_Concept DoOnClose>
-void WebsocketClientSession<DoOnConnectType, DoOnRead, DoOnWrite, DoOnClose>::OnRead(error_code ec,
-                                                                                     std::size_t)
+void WebsocketClientSession<DoOnConnectType, DoOnRead, DoOnWrite, DoOnClose>::OnRead(error_code ec, std::size_t)
 {
     m_doOnRead(beast::buffer::to_string(m_buffer.data()), ec);
     if (ec)
+    {
         return;
+    }
 
     m_buffer.consume(m_buffer.size());
 
@@ -171,6 +170,8 @@ void WebsocketClientSession<DoOnConnectType, DoOnRead, DoOnWrite, DoOnClose>::On
 {
     m_doOnClose(ec);
     if (ec)
+    {
         return;
+    }
 }
-}  // namespace inklink_client_session
+} // namespace inklink::client_connector
