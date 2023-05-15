7ddd87b10affea7cca01318e8fa3336b83d69454
Gallaann <reindzer26rus@gmail.com>
2023-05-16 02:08:56 +0300

    ADD method for generating random position of vertexes

--- frontend/graph/src/GraphModel.cpp
+++ frontend/graph/src/GraphModel.cpp
@@ -1,10 +1,15 @@
 #include "GraphModel.h"
 
 #include <memory>
+#include <random>
 #include <string>
 
 namespace inklink::graph
 {
+enum{
+    kPosition = 300
+};
+
 // TODO should parse raw data to edges and unique vertexes, then add them to graph
 void GraphModel::addVertexesToGraph(std::vector<std::string> vertexes)
 {
@@ -27,7 +32,16 @@ std::vector<std::shared_ptr<IEdge>> GraphModel::getEdges()
     return m_edges;
 }
 
+// also would be used for shuffle in 'view'
 Position GraphModel::GeneratePosition()
 {
+    std::random_device seed;
+    std::mt19937 gen(seed());
+    std::uniform_int_distribution<> dis(-kPosition, kPosition);
+
+    Position temp;
+    temp.yPosition = dis(gen);
+    temp.xPosition = dis(gen);
+    return temp;
 }
 } // namespace inklink::graph
