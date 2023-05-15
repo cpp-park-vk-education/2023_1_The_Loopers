2c0276a7417bd113db337e36a73968d5b9920cfe
Gallaann <reindzer26rus@gmail.com>
2023-05-16 01:51:48 +0300

    ADD method to add vertexes to graph

---
+++ frontend/graph/src/GraphModel.cpp
@@ -0,0 +1,31 @@
+#include "GraphModel.h"
+
+#include <memory>
+#include <string>
+
+namespace inklink::graph
+{
+// TODO should parse raw data to edges and unique vertexes, then add them to graph
+void GraphModel::addVertexesToGraph(std::vector<std::string> vertexes)
+{
+    for (std::string vertexName : vertexes)
+    {
+        auto vertexPosition = GeneratePosition();
+
+        std::shared_ptr<IVertex> newVertex = std::make_shared<IVertex>(vertexName, vertexPosition);
+        m_uniqueVertexes.push_back(newVertex);
+    }
+}
+
+std::vector<IVertex*> GraphModel::getUniqueVertexes()
+{
+}
+
+std::vector<IEdge*> GraphModel::getEdges()
+{
+}
+
+Position GraphModel::GeneratePosition()
+{
+}
+} // namespace inklink::graph
