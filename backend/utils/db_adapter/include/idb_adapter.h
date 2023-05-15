3b1f21c1375af19265ad1c5a9bfe8d6f0fa0bdea
kingjoram <alex.v.danilchenko@yandex.ru>
2023-05-15 21:27:06 +0300

    INKLB-007 REF:database:db_adapter:refactor idb_adapter.h

--- backend/utils/db_adapter/include/idb_adapter.h
+++ backend/utils/db_adapter/include/idb_adapter.h
@@ -10,10 +10,15 @@ class IDBAdapter
 public:
     virtual ~IDBAdapter() = default;
 
-    virtual void Insert(const std::string&) = 0;
-    virtual void Update(const std::string&) = 0;
-    virtual std::string Select(const std::string&) = 0;
-    virtual void Delete(const std::string&) = 0;
-    virtual void Connect(const std::string&) = 0;
+    virtual void Insert(const std::string& insertPart, const std::string& valuesPart) = 0;
+
+    virtual void Update(const std::string& updatePart, const std::string& setPart, const std::stirng wherePart) = 0;
+
+    virtual std::string Select(const std::string& selectPart, const std::string& fromPart, const std::string& wherePart,
+                               const std::string& orderPart) = 0;
+
+    virtual void Delete(const std::string& deletePart, const std::string& fromPart, const std::string& wherePart) = 0;
+
+    virtual void Connect(const std::string& connectionString) = 0;
 };
 } // namespace inklink::db_adapter
