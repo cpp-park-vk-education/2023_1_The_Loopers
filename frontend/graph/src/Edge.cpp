5eddecb3d80c22ed192d91837eeb7989488a2b0e
Gallaann <reindzer26rus@gmail.com>
2023-05-20 02:40:13 +0300

    ADD implementation for source vertex getter

--- frontend/graph/src/Edge.cpp
+++ frontend/graph/src/Edge.cpp
@@ -2,8 +2,13 @@
 
 namespace inklink::graph
 {
+const std::shared_ptr<IVertex> IEdge::GetSourceVertex() const
+{
+    return m_sourceVertex;
+}
+
 const std::shared_ptr<IVertex> IEdge::GetDestinationVertex() const
 {
     return m_destinationVertex;
 }
-}
+} // namespace inklink::graph
