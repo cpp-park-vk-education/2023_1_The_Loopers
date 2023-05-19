82b8d29dac2b594ac4cc5b9ac343628842f6063d
Gallaann <reindzer26rus@gmail.com>
2023-05-19 20:31:29 +0300

    FIX problem with override

--- frontend/graph/include/GraphModel.h
+++ frontend/graph/include/GraphModel.h
@@ -11,6 +11,10 @@
 
 namespace inklink::graph
 {
+
+class IEdge;
+class IVertex;
+
 class GraphModel : public IGraphModel
 {
 public:
@@ -19,22 +23,14 @@ public:
     std::vector<std::shared_ptr<IEdge>> GetEdges() const override;
 
 private:
-    struct NamingsEdge
-    {
-        std::string source;
-        std::string destination;
-    };
-
-    // first vertex in vector used as center vertex
-    std::set<std::shared_ptr<IVertex>> m_uniqueVertexes;
+    std::set<std::shared_ptr<IVertex>> m_uniqueVertexes; // first vertex in container used as center vertex
     std::vector<std::shared_ptr<IEdge>> m_edges;
 
-    Position GeneratePosition();                             // generates random position for vertex
     std::set<std::shared_ptr<IVertex>> MakeUniqueVertexes() override; // parse edges and find unique vertexes
     void FillUniqueVertexes(std::set<std::shared_ptr<IVertex>>) override;
     void FillEdges(const std::vector<NamingsEdge> namings) override;
-    void SetRandomPositions() ;
-    std::vector<NamingsEdge> ParseRawData(std::string& rawEdgeString) override; // parse raw string and makes
-                                                                       // edges with vertex namings
+    Position GeneratePosition(); // generates random position for vertex
+    void SetRandomPositions();
+    std::vector<NamingsEdge> ParseRawData(std::string& rawEdgeString);
 };
 } // namespace inklink::graph
--- frontend/graph/include/IGraphModel.h
+++ frontend/graph/include/IGraphModel.h
@@ -8,31 +8,30 @@
 #include <string>
 #include <vector>
 
+namespace inklink::graph
+{
 class IEdge;
 class IVertex;
 
-namespace inklink::graph
-{
 class IGraphModel
 {
 public:
-    virtual void FillGraphByEdges(std::string &rawData) = 0;
-    virtual std::set<std::shared_ptr<IVertex>> GetUniqueVertexes() const = 0;
-    virtual std::vector<std::shared_ptr<IEdge>> GetEdges() const = 0;
-
-private:
     struct NamingsEdge
     {
         std::string source;
         std::string destination;
     };
 
+    virtual void FillGraphByEdges(std::string &rawData) = 0;
+    virtual std::set<std::shared_ptr<IVertex>> GetUniqueVertexes() const = 0;
+    virtual std::vector<std::shared_ptr<IEdge>> GetEdges() const = 0;
+
+private:
     std::set<std::shared_ptr<IVertex>> m_uniqueVertexes;
     std::vector<std::shared_ptr<IEdge>> m_edges;
 
     virtual std::set<std::shared_ptr<IVertex>> MakeUniqueVertexes() = 0;
     virtual void FillUniqueVertexes(std::set<std::shared_ptr<IVertex>>) = 0;
     virtual void FillEdges(const std::vector<NamingsEdge> namings) = 0;
-    virtual std::vector<NamingsEdge> ParseRawData(std::string &rawEdgeString) = 0;
 };
-}  // namespace inklink::graph
+} // namespace inklink::graph
