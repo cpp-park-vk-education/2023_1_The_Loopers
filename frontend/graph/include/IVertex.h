3c9b8456d9c87c3cb60ed4d8485b07e8e18907b0
Gallaann <reindzer26rus@gmail.com>
2023-05-20 02:09:46 +0300

    REF IVertex so GraphModel methods work without errors

--- frontend/graph/include/IVertex.h
+++ frontend/graph/include/IVertex.h
@@ -9,26 +9,25 @@
 namespace inklink::graph
 {
 
-struct Position{
-    std::size_t xPosition;
-    std::size_t yPosition;
-};
-
 class IVertex
 {
 public:
+    struct Position
+    {
+        std::size_t xPosition;
+        std::size_t yPosition;
+    };
+
+    explicit IVertex(std::string& name) : m_name(name){};
+
     void calculateForces();
     bool advancePosition();
-    std::string getName();
-
-    Position getCurrentPosition();
-//    Position getNewPosition();
-    void setCurrentPosition(Position);
-//    void setNewPosition(Position);
+    [[nodiscard]] std::string getName();
+    [[nodiscard]] Position GetCurrentPosition();
+    void SetCurrentPosition(Position);
 
 private:
     std::string m_name;
     Position m_currentPosition;
-//    Position m_newPosition;
 };
 } // namespace inklink::graph
