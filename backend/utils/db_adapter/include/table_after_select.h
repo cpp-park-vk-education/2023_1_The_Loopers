e95d90a84c07abece99d8345d221faf611b8a860
kingjoram <alex.v.danilchenko@yandex.ru>
2023-05-20 02:51:04 +0300

    INKLB-008:NEW:database:create table_after_select.h

---
+++ backend/utils/db_adapter/include/table_after_select.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <vector>
+#include <string>
+
+
+namespace inklink::db_controller
+{
+struct TableAfterSelect
+{
+public:
+    [[nodiscard]] std::vector<std::string> operator[](const int iterator) const
+    {
+        return table[iterator];
+    }
+
+    void PushBackRow(std::vector<std::string>& row)
+    {
+        table.push_back(row);
+    }
+
+private:
+    std::vector<std::vector<std::string>> table;
+};
+} // namespace inklink::db_controller
