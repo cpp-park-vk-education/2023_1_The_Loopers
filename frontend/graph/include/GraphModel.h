c06d96dc92b64e0ae7a2040076cc3b5490035c19
Gallaann <reindzer26rus@gmail.com>
2023-05-19 22:19:00 +0300

    ADD nodiscard attribute

--- frontend/graph/include/GraphModel.h
+++ frontend/graph/include/GraphModel.h
@@ -19,18 +19,18 @@ class GraphModel : public IGraphModel
 {
 public:
     void FillGraphByEdges(std::string& rawData) override;
-    std::set<std::shared_ptr<IVertex>> GetUniqueVertexes() const override;
-    std::vector<std::shared_ptr<IEdge>> GetEdges() const override;
+    [[nodiscard]] std::set<std::shared_ptr<IVertex>> GetUniqueVertexes() const override;
+    [[nodiscard]] std::vector<std::shared_ptr<IEdge>> GetEdges() const override;
 
 private:
     std::set<std::shared_ptr<IVertex>> m_uniqueVertexes; // first vertex in container used as center vertex
     std::vector<std::shared_ptr<IEdge>> m_edges;
 
-    std::set<std::shared_ptr<IVertex>> MakeUniqueVertexes() override; // parse edges and find unique vertexes
+    [[nodiscard]] std::set<std::shared_ptr<IVertex>> MakeUniqueVertexes() override; // parse edges and find unique vertexes
     void FillUniqueVertexes(std::set<std::shared_ptr<IVertex>>) override;
     void FillEdges(const std::vector<NamingsEdge> namings) override;
-    IVertex::Position GeneratePosition(); // generates random position for vertex
+    [[nodiscard]] IVertex::Position GeneratePosition(); // generates random position for vertex
     void SetRandomPositions();
-    std::vector<NamingsEdge> ParseRawData(std::string& rawEdgeString);
+    [[nodiscard]] std::vector<NamingsEdge> ParseRawData(std::string& rawEdgeString);
 };
 } // namespace inklink::graph
--- frontend/graph/include/IGraphModel.h
+++ frontend/graph/include/IGraphModel.h
@@ -23,14 +23,14 @@ public:
     };
 
     virtual void FillGraphByEdges(std::string &rawData) = 0;
-    virtual std::set<std::shared_ptr<IVertex>> GetUniqueVertexes() const = 0;
-    virtual std::vector<std::shared_ptr<IEdge>> GetEdges() const = 0;
+    [[nodiscard]] virtual std::set<std::shared_ptr<IVertex>> GetUniqueVertexes() const = 0;
+    [[nodiscard]] virtual std::vector<std::shared_ptr<IEdge>> GetEdges() const = 0;
 
 private:
     std::set<std::shared_ptr<IVertex>> m_uniqueVertexes;
     std::vector<std::shared_ptr<IEdge>> m_edges;
 
-    virtual std::set<std::shared_ptr<IVertex>> MakeUniqueVertexes() = 0;
+    [[nodiscard]] virtual std::set<std::shared_ptr<IVertex>> MakeUniqueVertexes() = 0;
     virtual void FillUniqueVertexes(std::set<std::shared_ptr<IVertex>>) = 0;
     virtual void FillEdges(const std::vector<NamingsEdge> namings) = 0;
 };
