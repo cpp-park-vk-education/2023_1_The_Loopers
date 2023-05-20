57e4e511c1763f975d63bed3c3cec3306886cda7
Gallaann <reindzer26rus@gmail.com>
2023-05-21 02:54:58 +0300

    FIX lib linking with Qt

--- cmake/apply_qt.cmake
+++ cmake/apply_qt.cmake
@@ -3,8 +3,11 @@ set(QT_VERSION 6)
 set(REQUIRED_LIBS Core Gui Widgets)
 set(REQUIRED_LIBS_QUALIFIED Qt6::Core Qt6::Gui Qt6::Widgets)
 
-find_package(Qt${QT_VERSION} REQUIRED COMPONENTS ${REQUIRED_LIBS})
-
-set(QT_COMPONENTS ${REQUIRED_LIBS_QUALIFIED})
+find_package(Qt6 REQUIRED COMPONENTS ${REQUIRED_LIBS})
 
 qt_standard_project_setup()
+
+target_link_libraries(${GRAPH} PRIVATE ${REQUIRED_LIBS_QUALIFIED})
+target_link_libraries(${MENU} PRIVATE ${REQUIRED_LIBS_QUALIFIED})
+target_link_libraries(${DRAW} PRIVATE ${REQUIRED_LIBS_QUALIFIED})
+target_link_libraries(${AUTH} PRIVATE ${REQUIRED_LIBS_QUALIFIED})
