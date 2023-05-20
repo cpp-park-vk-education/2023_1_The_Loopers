2c73e7dda2df63e4095f50afbfb7e3f473b86a80
kingjoram <alex.v.danilchenko@yandex.ru>
2023-05-20 17:42:25 +0300

    INKLB-008:REF:db_controller:CI fix

--- backend/utils/db_adapter/include/storage_db_controller.h
+++ backend/utils/db_adapter/include/storage_db_controller.h
@@ -18,7 +18,7 @@ class StorageDbController final
 public:
     explicit void SetAdapter(DbAdapterBase& adapter);
 
-    void Connect(const std::string& connectionString) = 0;
+    void Connect(const std::string& connectionString);
 
     [[nodiscard]] filesystem::path GetFilePath(std::string& fileName, std::string& login) const;
     [[nodiscard]] std::string GetGraphArcs(std::string& vertexName, std::string& sessionId) const;
