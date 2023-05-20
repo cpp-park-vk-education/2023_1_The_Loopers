01847279bc58adcf6ebdf3dc476928ff7f585673
Gallaann <reindzer26rus@gmail.com>
2023-05-21 02:08:33 +0300

    ADD variables in CMake for qt

--- cmake/apply_qt.cmake
+++ cmake/apply_qt.cmake
@@ -1,11 +1,10 @@
 # find third party libraries (qt)
-find_package(Qt6 REQUIRED COMPONENTS Core Gui Widgets)
+set(QT_VERSION 6)
+set(REQUIRED_LIBS Core Gui Widgets)
+set(REQUIRED_LIBS_QUALIFIED Qt6::Core Qt6::Gui Qt6::Widgets)
 
+find_package(Qt${QT_VERSION} REQUIRED COMPONENTS ${REQUIRED_LIBS})
 
-set(QT_COMPONENTS
-        "Qt6::Core"
-        "Qt6::Gui"
-        "Qt6::Widgets"
-        )
+set(QT_COMPONENTS ${REQUIRED_LIBS_QUALIFIED})
 
 qt_standard_project_setup()
