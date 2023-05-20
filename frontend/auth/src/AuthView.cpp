5aead17aa19c96667b5bb99fd89479225dec5b8e
Alexander Zhokhov <a.zhokhov@yandex.ru>
2023-05-20 23:57:51 +0300

    Roflan commit

--- frontend/auth/src/AuthView.cpp
+++ frontend/auth/src/AuthView.cpp
@@ -1,1 +1,13 @@
+#include "AuthView.hpp"
+
+#include <QApplication>
+#include <QPushButton>
+
+int main(int argc, char *argv[]) {
+    QApplication a(argc, argv);
+    QPushButton button("Hello world!", nullptr);
+    button.resize(200, 100);
+    button.show();
+    return QApplication::exec();
+}
 
