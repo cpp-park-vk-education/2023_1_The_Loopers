bea552b2c67b666795bc1518674735999178e26d
Gallaann <reindzer26rus@gmail.com>
2023-05-21 17:11:35 +0300

    ADD shape for vertex

--- frontend/graph/src/VertexView.cpp
+++ frontend/graph/src/VertexView.cpp
@@ -9,4 +9,11 @@ VertexView::VertexView(GraphView *graphView) : m_graph(graphView)
     setCacheMode(DeviceCoordinateCache);
     setZValue(1);
 }
+
+QPainterPath VertexView::shape() const
+{
+    QPainterPath path;
+    path.addEllipse(-10, -10, 20, 20);
+    return path;
+}
 } // namespace inklink::graph
