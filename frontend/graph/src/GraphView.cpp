e96f7910cd1a428b72a268c85404c6a6768484e1
Gallaann <reindzer26rus@gmail.com>
2023-05-21 03:05:43 +0300

    ADD function to draw background

--- frontend/graph/src/GraphView.cpp
+++ frontend/graph/src/GraphView.cpp
@@ -1,3 +1,11 @@
-//
-// Created by gallaann on 21.05.23.
-//
+#include "GraphView.h"
+
+namespace inklink::graph
+{
+void GraphView::drawBackground(QPainter *painter, const QRectF &rect)
+{
+    painter->setPen(Qt::NoPen);
+    painter->setBrush(QColor(30, 30, 30));
+    painter->drawRect(rect);
+}
+} // namespace inklink::graph
