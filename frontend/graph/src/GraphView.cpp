4537f94fefd4e0b031ecbdb4cc9fbb7b79ad4a00
Gallaann <reindzer26rus@gmail.com>
2023-05-21 03:19:38 +0300

    ADD method to scale view

--- frontend/graph/src/GraphView.cpp
+++ frontend/graph/src/GraphView.cpp
@@ -2,6 +2,19 @@
 
 namespace inklink::graph
 {
+void GraphView::wheelEvent(QWheelEvent *event)
+{
+    scaleView(pow(2., event->angleDelta().y() / 400.0));
+}
+
+void GraphView::scaleView(qreal scaleFactor) {
+    qreal factor = transform().scale(scaleFactor, scaleFactor).mapRect(QRectF(0, 0, 1, 1)).width();
+    if (factor < 0.5 || factor > 5)
+        return;
+
+    scale(scaleFactor, scaleFactor);
+}
+
 void GraphView::drawBackground(QPainter *painter, const QRectF &rect)
 {
     painter->setPen(Qt::NoPen);
