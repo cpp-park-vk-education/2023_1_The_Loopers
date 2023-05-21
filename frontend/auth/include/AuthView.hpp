140ac19fb9e708c57fb27e124075f1e6c279a84b
Alexander Zhokhov <a.zhokhov@yandex.ru>
2023-05-21 21:56:31 +0300

    Add AuthView without signals and slots

--- frontend/auth/include/AuthView.hpp
+++ frontend/auth/include/AuthView.hpp
@@ -1,2 +1,24 @@
 #pragma once
 
+#include <QDialog>
+#include <QGraphicsView>
+#include <QWidget>
+
+class AuthView : public QGraphicsView {
+    //    Q_OBJECT
+public:
+    AuthView(QWidget *parent = nullptr);
+
+    ~AuthView() {}
+
+protected:
+    void drawBackground(QPainter *painter, const QRectF &rect) override;
+};
+
+class AuthDialog : public QDialog {
+    //    Q_OBJECT
+public:
+    AuthDialog(QWidget *parent = nullptr);
+
+    ~AuthDialog() {}
+};
--- frontend/auth/src/AuthView.cpp
+++ frontend/auth/src/AuthView.cpp
@@ -1,13 +1,59 @@
 #include "AuthView.hpp"
 
-#include <QApplication>
+#include <QDialog>
+#include <QGraphicsView>
+#include <QLabel>
+#include <QLineEdit>
 #include <QPushButton>
+#include <QVBoxLayout>
+#include <QWidget>
 
-int main(int argc, char *argv[]) {
-    QApplication a(argc, argv);
-    QPushButton button("Hello world!", nullptr);
-    button.resize(200, 100);
-    button.show();
-    return QApplication::exec();
+AuthView::AuthView(QWidget *parent) : QGraphicsView(parent)
+{
+    QGraphicsScene *scene = new QGraphicsScene(this);
+    scene->setSceneRect(-640, -460, 1280, 920);
+    setScene(scene);
+    setFrameShape(QFrame::NoFrame);
+    setRenderHint(QPainter::Antialiasing);
+    setTransformationAnchor(AnchorUnderMouse);
+    this->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    this->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
 }
 
+void AuthView::drawBackground(QPainter *painter, const QRectF &rect) {
+    painter->setPen(Qt::NoPen);
+    painter->setBrush(QColor(255, 255, 255));
+    painter->drawRect(rect);
+}
+
+AuthDialog::AuthDialog(QWidget *parent) : QDialog(parent){
+    setWindowTitle("Login");
+
+    QLineEdit *usernameLine = new QLineEdit(this);
+    QLineEdit *passwordLine = new QLineEdit(this);
+
+    QLabel* usernameLabel = new QLabel(tr("Username"), this);
+    QLabel* passwordLabel = new QLabel(tr("Password"), this);
+
+    QPushButton *acceptButton = new QPushButton(tr("Log in"), this);
+    acceptButton->setAutoDefault(false);
+
+    QPushButton *registrationButton = new QPushButton(tr("Registration"), this);
+    registrationButton->setAutoDefault(false);
+
+    QHBoxLayout* buttonsLayout = new QHBoxLayout;
+    buttonsLayout->setAlignment(Qt::AlignCenter);
+    buttonsLayout->addWidget(acceptButton);
+    buttonsLayout->addWidget(registrationButton);
+
+    QVBoxLayout* mainLayout = new QVBoxLayout(this);
+    mainLayout->addWidget(usernameLabel);
+    mainLayout->addWidget(usernameLine);
+
+    mainLayout->addWidget(passwordLabel);
+    mainLayout->addWidget(passwordLine);
+
+    mainLayout->addLayout(buttonsLayout);
+
+    setLayout(mainLayout);
+}
