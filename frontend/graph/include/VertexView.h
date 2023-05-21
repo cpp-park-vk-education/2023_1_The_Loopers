03393713bf30df96ddeb56ce1ed6e2bbdf6698e8
Gallaann <reindzer26rus@gmail.com>
2023-05-21 17:05:07 +0300

    ADD header for vertex view

---
+++ frontend/graph/include/VertexView.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <QGraphicsItem>
+
+class GraphView;
+
+class VertexView : public QGraphicsItem
+{
+public:
+    VertexView(GraphView* graphView);
+
+    void paint(QPainter* painter, const QStyleOptionGraphicsItem* option, QWidget* widget) override;
+    QRectF boundingRect() const override;
+    QPainterPath shape() const override;
+
+protected:
+    QVariant itemChange(GraphicsItemChange change, const QVariant& value) override;
+    void mousePressEvent(QGraphicsSceneMouseEvent* event) override;
+    void mouseReleaseEvent(QGraphicsSceneMouseEvent* event) override;
+
+private:
+    GraphView* m_graph;
+};
---
+++ frontend/graph/src/VertexView.cpp
@@ -0,0 +1,1 @@
+#include "VertexView.h"
