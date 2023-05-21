7be061df9899f62e7252cdad7385015194d59a8f
Gallaann <reindzer26rus@gmail.com>
2023-05-21 17:09:41 +0300

    ADD constructor for vertex view

--- frontend/graph/src/VertexView.cpp
+++ frontend/graph/src/VertexView.cpp
@@ -1,1 +1,12 @@
 #include "VertexView.h"
+
+namespace inklink::graph
+{
+VertexView::VertexView(GraphView *graphView) : graph(graphView)
+{
+    setFlag(ItemIsMovable);
+    setFlag(ItemSendsGeometryChanges);
+    setCacheMode(DeviceCoordinateCache);
+    setZValue(1);
+}
+} // namespace inklink::graph
