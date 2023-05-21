e079fbb511ead5c374ad9875e2f4689dbcfe8d62
Gallaann <reindzer26rus@gmail.com>
2023-05-21 20:55:50 +0300

    currently commented macros, so can execute program

--- frontend/graph/include/GraphView.h
+++ frontend/graph/include/GraphView.h
@@ -1,16 +1,18 @@
 #pragma once
 
 #include <QGraphicsView>
-#include <QKeyEvent>
+#include <QWheelEvent>
 
-// TODO: (zakharov) add function to add vertex and edge to the view
+// TODO: (zakharov) add function to add vertex and edge to the view, make something with Q_OBJECT macros
 namespace inklink::graph
 {
 class GraphView : public QGraphicsView
 {
-    Q_OBJECT
+    //    Q_OBJECT
 public:
     GraphView(QWidget* parent = nullptr);
+
+    ~GraphView() = default;
 
 protected:
     void wheelEvent(QWheelEvent* event) override;
