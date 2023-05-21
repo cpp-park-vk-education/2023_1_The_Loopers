1b007849821754ba823cb8249dca407e7460d1be
Gallaann <reindzer26rus@gmail.com>
2023-05-21 17:33:52 +0300

    ADD method for drawing vertex

--- frontend/graph/include/VertexView.h
+++ frontend/graph/include/VertexView.h
@@ -1,9 +1,10 @@
 #pragma once
 
 #include <QGraphicsItem>
+#include <QGraphicsSceneMouseEvent>
+#include <QStyleOption>
 
 class GraphView;
-
 
 namespace inklink::graph
 {
--- frontend/graph/src/VertexView.cpp
+++ frontend/graph/src/VertexView.cpp
@@ -1,8 +1,12 @@
 #include "VertexView.h"
 
+#include <QPainter>
+#include <QRadialGradient>
+#include <QStyle>
+
 namespace inklink::graph
 {
-VertexView::VertexView(GraphView *graphView) : m_graph(graphView)
+VertexView::VertexView(GraphView* graphView) : m_graph(graphView)
 {
     setFlag(ItemIsMovable);
     setFlag(ItemSendsGeometryChanges);
@@ -10,10 +14,29 @@ VertexView::VertexView(GraphView *graphView) : m_graph(graphView)
     setZValue(1);
 }
 
+void VertexView::paint(QPainter* painter, const QStyleOptionGraphicsItem* option, QWidget*)
+{
+    QRadialGradient gradient(0, 0, 10);
+    if (option->state & QStyle::State_Sunken)
+    {
+        gradient.setColorAt(1, QColor(124, 90, 237).darker(100));
+        gradient.setColorAt(0, QColor(124, 90, 237).lighter(100));
+    }
+    else
+    {
+        gradient.setColorAt(0, QColor(218, 218, 218));
+        gradient.setColorAt(1, QColor(218, 218, 218));
+    }
+
+    painter->setBrush(gradient);
+    painter->setPen(QPen(gradient, 0));
+    painter->drawEllipse(-10, -10, 20, 20);
+}
+
 QRectF VertexView::boundingRect() const
 {
     qreal penWidth = 2;
-    return QRectF( -10 - penWidth/2, -10 - penWidth/2, 23 + penWidth/2, 23 + penWidth/2);
+    return QRectF(-10 - penWidth / 2, -10 - penWidth / 2, 23 + penWidth / 2, 23 + penWidth / 2);
 }
 
 QPainterPath VertexView::shape() const
