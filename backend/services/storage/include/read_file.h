d17e1d82cb72aa225dc4ace000ae9957e928771e
kingjoram <alex.v.danilchenko@yandex.ru>
2023-05-22 07:34:21 +0300

    INKLB-005:REF:file_system:after review changes

--- backend/services/storage/include/read_file.h
+++ backend/services/storage/include/read_file.h
@@ -10,7 +10,7 @@ namespace inklink_file_holder
 class ReadFile final
 {
 public:
-    [[nodiscard]] std::string GetFile(const std::filesystem::path& filePath) const noexcept;
+    [[nodiscard]] std::string GetFile(const std::filesystem::path& filePath) const  noexcept;
 };
 }  // namespace inklink_file_holder
 #endif  // _BACK_SERVICES_STORAGE_INCLUDE_READ_FILE_H_
