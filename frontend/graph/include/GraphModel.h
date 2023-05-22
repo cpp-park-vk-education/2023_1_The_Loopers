ded096a69983dc0422bab531b94d9012c7f970a2
Gallaann <reindzer26rus@gmail.com>
2023-05-22 03:07:54 +0300

    FIX forward declaration
    * added one block for forward declaration and another for regular class declaration
    * removed includes of forward declared classes

--- frontend/graph/include/GraphModel.h
+++ frontend/graph/include/GraphModel.h
@@ -1,8 +1,6 @@
 #pragma once
 
-#include "IEdge.h"
 #include "IGraphModel.h"
-#include "IVertex.h"
 
 #include <memory>
 #include <set>
@@ -11,10 +9,11 @@
 
 namespace inklink::graph
 {
-
+// forward declaration
 class IEdge;
 class IVertex;
 
+// class declaration
 class GraphModel : public IGraphModel
 {
 public:
@@ -26,7 +25,8 @@ private:
     std::set<std::shared_ptr<IVertex>> m_uniqueVertexes; // first vertex in container used as center vertex
     std::vector<std::shared_ptr<IEdge>> m_edges;
 
-    [[nodiscard]] std::set<std::shared_ptr<IVertex>> MakeUniqueVertexes() override; // parse edges and find unique vertexes
+    [[nodiscard]] std::set<std::shared_ptr<IVertex>>
+    MakeUniqueVertexes() override; // parse edges and find unique vertexes
     void FillUniqueVertexes(std::set<std::shared_ptr<IVertex>>) override;
     void FillEdges(const std::vector<NamingsEdge> namings) override;
     [[nodiscard]] IVertex::Position GeneratePosition(); // generates random position for vertex
