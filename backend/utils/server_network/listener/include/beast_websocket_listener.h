94bc6167060863a2717049638157d86ead563915
Alexander Novak <sasha_7tdd7@mail.ru>
2023-05-23 20:30:32 +0300

    network: template classes to headers

--- backend/utils/server_network/listener/include/beast_websocket_listener.h
+++ backend/utils/server_network/listener/include/beast_websocket_listener.h
@@ -55,3 +55,91 @@ private:
     AcceptCallback m_acceptCallback;
 };
 } // namespace inklink::server_network
+
+namespace inklink::server_network
+{
+// Accepts incoming connections and launches the sessions
+template <AcceptCallbackConcept AcceptCallback>
+inline BeastWebsocketListener<AcceptCallback>::BeastWebsocketListener(boost::asio::io_context& ioContext,
+                                                                      const tcp::endpoint& endpoint,
+                                                                      std::unique_ptr<ISessionsFactory> factory,
+                                                                      AcceptCallback acceptCallback)
+        : IListener{std::move(factory)}, m_ioContext{ioContext}, m_acceptor{ioContext}, m_acceptCallback{acceptCallback}
+{
+    error_code errorCode;
+
+    // Open the acceptor
+    if (m_acceptor.open(endpoint.protocol(), errorCode); errorCode)
+    {
+        TerminateOnFail(errorCode, "open ");
+        return;
+    }
+
+    // Allow address reuse
+    m_acceptor.set_option(boost::asio::socket_base::reuse_address(true), errorCode);
+    if (errorCode)
+    {
+        TerminateOnFail(errorCode, "set_option ");
+        return;
+    }
+
+    // Bind to the server address
+    if (m_acceptor.bind(endpoint, errorCode); errorCode)
+    {
+        TerminateOnFail(errorCode, "bind ");
+        return;
+    }
+
+    // Start listening for connections
+    m_acceptor.listen(boost::asio::socket_base::max_listen_connections, errorCode);
+    if (errorCode)
+    {
+        TerminateOnFail(errorCode, "listen ");
+        return;
+    }
+}
+
+// Start accepting incoming connections
+template <AcceptCallbackConcept AcceptCallback>
+inline void BeastWebsocketListener<AcceptCallback>::AsyncRun()
+{
+    if (m_acceptor.is_open())
+    {
+        DoAccept();
+    }
+}
+
+template <AcceptCallbackConcept AcceptCallback>
+inline void BeastWebsocketListener<AcceptCallback>::DoAccept()
+{
+    m_acceptor.async_accept(boost::asio::make_strand(m_ioContext),
+                            boost::beast::bind_front_handler(&BeastWebsocketListener<AcceptCallback>::OnAccept,
+                                                             this->shared_from_this()));
+}
+
+template <AcceptCallbackConcept AcceptCallback>
+inline void BeastWebsocketListener<AcceptCallback>::OnAccept(error_code ec, tcp::socket socket)
+{
+    if (ec)
+    {
+        m_acceptCallback(ec, nullptr);
+    }
+    else
+    {
+        // Create the session and run it
+        auto session = m_factory->GetSession(std::move(socket));
+        m_acceptCallback(ec, session.get());
+        session->RunAsync();
+    }
+
+    DoAccept();
+}
+
+// Report a Failure
+template <AcceptCallbackConcept AcceptCallback>
+inline void BeastWebsocketListener<AcceptCallback>::TerminateOnFail(error_code ec, const std::string&)
+{
+    // TODO (a.novak) custom error or new error_code with additional info
+    throw ec;
+}
+} // namespace inklink::server_network
--- backend/utils/server_network/sessions_factory/include/websocket_session_factory.h
+++ backend/utils/server_network/sessions_factory/include/websocket_session_factory.h
@@ -2,6 +2,7 @@
 
 #include "inklink/server_network/callback_concepts.h"
 #include "isessions_factory.h"
+#include "websocket_service_session.h"
 
 #include <concepts>
 #include <functional>
@@ -50,4 +51,33 @@ private:
     AcceptCallback m_acceptCallback;
     WriteCallback m_writeCallback;
 };
+} // namespace inklink::server_network
+
+namespace inklink::server_network
+{
+// clang-format off
+template <StringErrorCodeSessionCallbackConcept ReadCallback, ErrorCodeAndSessionCallbackConcept AcceptCallback,
+          ErrorCodeCallbackConcept WriteCallback> inline
+WebsocketSessionsFactory<ReadCallback, AcceptCallback, WriteCallback>::WebsocketSessionsFactory(
+        std::shared_ptr<InternalSessionsManager> manager, 
+        std::shared_ptr<IAuthorizer> auth, 
+        ReadCallback readCallback,
+        AcceptCallback acceptCallback, 
+        WriteCallback writeCallback) noexcept
+        : ISessionsFactory{std::move(manager), std::move(auth)},
+          m_readCallback{readCallback},
+          m_acceptCallback{acceptCallback}, 
+          m_writeCallback{writeCallback}
+{
+}
+// clang-format on
+
+template <StringErrorCodeSessionCallbackConcept ReadCallback, ErrorCodeAndSessionCallbackConcept AcceptCallback,
+          ErrorCodeCallbackConcept WriteCallback>
+inline std::shared_ptr<IServiceSession>
+WebsocketSessionsFactory<ReadCallback, AcceptCallback, WriteCallback>::GetSession(boost::asio::ip::tcp::socket&& socket)
+{
+    return std::make_shared<WebsocketServiceSession<ReadCallback, AcceptCallback, WriteCallback>>(
+            std::move(socket), m_readCallback, m_acceptCallback, m_writeCallback);
+}
 } // namespace inklink::server_network
