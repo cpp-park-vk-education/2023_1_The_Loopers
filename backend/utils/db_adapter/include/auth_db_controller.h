ffd2267d1aee9bdbad9b335256839e8d68a9f0cf
kingjoram <alex.v.danilchenko@yandex.ru>
2023-05-26 13:43:54 +0300

    INKLB-019:NEW:auth_handler:add auth_db_controller

---
+++ backend/utils/db_adapter/include/auth_db_controller.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "iauth_db_controller.h"
+
+#include <string>
+
+namespace inklink::db_controller
+{
+class AuthDbController : public IAuthDbController
+{
+public:
+    using DbRow = std::vector<std::string>;
+    using DbTable = std::vector<DbRow>;
+    
+public:
+    void SetAdapter(DbAdapterBase& adapter) override;
+
+    void Run(const std::string& connectionString) override;
+
+    bool InsertUser(const std::string& login, const std::string& password) const override;
+
+    [[nodiscard]] std::string GetPassword(const std::string& login) const override;
+};
+} // namespace inklink::db_controller
---
+++ backend/utils/db_adapter/include/iauth_db_controller.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "idb_adapter.h"
+
+#include <string>
+
+namespace inklink::db_controller
+{
+class IAuthDbController
+{
+public:
+    virtual void SetAdapter(DbAdapterBase& adapter) = 0;
+
+    virtual void Run(const std::string& connectionString) = 0;
+
+    virtual bool InsertUser(const std::string& login, const std::string& password) const = 0;
+
+    virtual [[nodiscard]] std::string GetPassword(const std::string& login) const = 0;
+
+protected:
+    DbAdapterBase& m_adapter;
+};
+} // namespace inklink::db_controller
