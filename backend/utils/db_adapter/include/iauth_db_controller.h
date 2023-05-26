e3e7ae2affb083c179e38fd6c689eb9251fc1742
kingjoram <alex.v.danilchenko@yandex.ru>
2023-05-26 13:04:13 +0300

    INKLB-019:NEW:auth_handler:add iauth_db_controller.h

---
+++ backend/utils/db_adapter/include/iauth_db_controller.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "idb_adapter.h"
+
+#include <filesystem>
+#include <string>
+
+namespace inklink::db_controller
+{
+class IAuthDbController
+{
+public:
+    virtual void SetAdapter(DbAdapterBase& adapter) = 0;
+
+    virtual void Run(const std::string& connectionString) = 0;
+
+    virtual void InsertUser(const std::string& login, const std::string& password) = 0;
+
+    virtual std::string GetPassword(const std::string& login) = 0;
+private:
+    DbAdapterBase& m_adapter;
+};
+} // namespace inklink::db_controller
