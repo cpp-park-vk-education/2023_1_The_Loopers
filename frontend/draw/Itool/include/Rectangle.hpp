d5f8355d2a35379e9731bd571cc1c44690a56df9
Alexander Zhokhov <a.zhokhov@yandex.ru>
2023-05-27 17:46:41 +0300

    Make storage QVariant and realization for this

--- frontend/draw/Itool/include/Rectangle.hpp
+++ frontend/draw/Itool/include/Rectangle.hpp
@@ -1,22 +1,30 @@
+#pragma once
+
 #include <QApplication>
 #include <QMouseEvent>
 #include <QPainter>
+#include <QVariant>
 #include <QWidget>
 
+#include <vector>
+
+namespace inklink::draw
+{
 class RectangleDrawingWidget : public QWidget
 {
 public:
-    RectangleDrawingWidget(QWidget *parent = nullptr);
+    RectangleDrawingWidget(QWidget* parent = nullptr);
 
 protected:
-    void paintEvent(QPaintEvent *event) override;
+    //    void paintEvent(QPaintEvent* event) override;
 
-    void mousePressEvent(QMouseEvent *event) override;
+    void mousePressEvent(QMouseEvent* event) override;
 
-    void mouseMoveEvent(QMouseEvent *event) override;
+    void mouseMoveEvent(QMouseEvent* event) override;
 
 private:
-    std::vector<QRect> rectangles; // Коллекция прямоугольников
-    QPoint startPoint;             // Начальная точка
-    QPoint endPoint;               // Конечная точка
+    std::vector<QVariant> items; // Коллекция элементов (прямоугольников)
+    QPoint startPoint;           // Начальная точка
+    QPoint endPoint;             // Конечная точка
 };
+} // namespace inklink::draw
--- frontend/draw/Itool/src/Rectangle.cpp
+++ frontend/draw/Itool/src/Rectangle.cpp
@@ -1,49 +1,55 @@
 #include "Rectangle.hpp"
 
-#include <QApplication>
-#include <QMouseEvent>
-#include <QPainter>
-#include <QWidget>
-
-RectangleDrawingWidget::RectangleDrawingWidget(QWidget *parent) : QWidget(parent)
+RectangleDrawingWidget::RectangleDrawingWidget(QWidget* parent) : QWidget(parent)
 {
     setMouseTracking(true); // Включаем отслеживание движения мыши
 }
 
-void RectangleDrawingWidget::paintEvent(QPaintEvent *event)
-{
-    QPainter painter(this);
-    painter.setRenderHint(QPainter::Antialiasing); // Сглаживание
+// void RectangleDrawingWidget::paintEvent(QPaintEvent* event) {
+//     QPainter painter(this);
+//     painter.setRenderHint(QPainter::Antialiasing); // Сглаживание
+//
+//     // Рисуем все сохраненные элементы
+//     for (const auto &item: items) {
+//         if (item.type() == QVariant::Rect) {
+//             QRect rect = item.value<QRect>();
+//             painter.drawRect(rect);
+//         }
+//     }
+// }
 
-    // Рисуем все сохраненные прямоугольники
-    for (const auto &rectangle : rectangles)
-    {
-        painter.drawRect(rectangle);
-    }
-}
-
-void RectangleDrawingWidget::mousePressEvent(QMouseEvent *event)
+void RectangleDrawingWidget::mousePressEvent(QMouseEvent* event)
 {
     if (event->button() == Qt::LeftButton)
     {
         startPoint = event->pos();
         endPoint = startPoint;
 
-        // Создаем прямоугольник с начальными координатами (точка - прямоугольник 1x1)
-        QRect rectangle(startPoint, QSize(1, 1));
-        rectangles.push_back(rectangle);
+        // Создаем элемент типа QRect с начальными координатами (ширина и высота равны 0)
+        QRect rect(startPoint, startPoint);
+        items.push_back(QVariant::fromValue(rect));
+
+        update(); // Перерисовываем холст
     }
 }
 
-void RectangleDrawingWidget::mouseMoveEvent(QMouseEvent *event)
+void RectangleDrawingWidget::mouseMoveEvent(QMouseEvent* event)
 {
     if (event->buttons() & Qt::LeftButton)
     {
         endPoint = event->pos();
 
-        // Обновляем размеры последнего прямоугольника в коллекции
-        QRect &lastRectangle = rectangles.back();
-        lastRectangle.setBottomRight(endPoint);
+        // Обновляем размеры последнего элемента в коллекции
+        if (!items.empty())
+        {
+            QVariant &lastItem = items.back();
+            if (lastItem.type() == QVariant::Rect)
+            {
+                QRect rect = lastItem.value<QRect>();
+                rect.setBottomRight(endPoint);
+                lastItem = QVariant::fromValue(rect);
+            }
+        }
 
         update(); // Перерисовываем холст
     }
