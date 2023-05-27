d32fb5cdf44d12ec4f46b3804c6a08578819187a
Gallaann <reindzer26rus@gmail.com>
2023-05-27 20:51:05 +0300

    ADD deserializer

--- frontend/draw/include/DrawModel.h
+++ frontend/draw/include/DrawModel.h
@@ -5,12 +5,15 @@
 #include <string>
 #include <vector>
 
+namespace {
+class DrawView;
+}
+
 namespace inklink::draw
 {
 class DrawModel
 {
 public:
-    void deserialize(std::string);
     std::string SerializeToSend(int actionID, int actionType, int figureID);
 
 protected:
@@ -19,7 +22,10 @@ protected:
     void addObject(size_t, std::string&, Point&, size_t);
 
 private:
+    DrawView* m_view;
     std::vector<ObjectWithAttributes*> m_objects;
     std::string m_filename;
+
+    void ParseToGet(const std::string& message);
 };
 } // namespace inklink::draw
--- frontend/draw/src/DrawModel.cpp
+++ frontend/draw/src/DrawModel.cpp
@@ -1,4 +1,5 @@
 #include "DrawModel.h"
+#include "DrawView.h"
 #include "data_container.h"
 #include "json_serializer.h"
 #include "websocket_client_session.h" // Sasha Novak says it should be in <> scopes, but i don't really know
@@ -14,5 +15,11 @@ std::string DrawModel::SerializeToSend(int actionId, int actionType, int figureI
     sendContainer["time"] = "now";
     sendContainer["figure_id"] = figureId;
     return JsonSerializer::SerializeAsString(sendContainer);
+}
+
+void DrawModel::ParseToGet(const std::string& message) {
+    DataContainer gotData = JsonSerializer::ParseFromString(message);
+    m_view->NotifyGotResultFromNetwork(true);
+    return;
 }
 } // namespace inklink::draw
