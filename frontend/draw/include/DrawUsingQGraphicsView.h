9033f715f7fdb913d2e8d5720c3ebd2968b77588
Gallaann <reindzer26rus@gmail.com>
2023-05-29 19:57:55 +0300

    draw view using QGraphicsView

---
+++ frontend/draw/include/DrawUsingQGraphicsView.h
@@ -0,0 +1,64 @@
+#pragma once
+
+#pragma once
+
+#include <QApplication>
+#include <QGraphicsScene>
+#include <QGraphicsView>
+#include <QGraphicsLineItem>
+#include <QMouseEvent>
+
+class DrawingView : public QGraphicsView
+{
+public:
+    DrawingView(QWidget* parent = nullptr)
+            : QGraphicsView(parent), scene(new QGraphicsScene(this)), drawing(false)
+    {
+        setScene(scene);
+    }
+
+protected:
+    void mousePressEvent(QMouseEvent* event) override
+    {
+        if (event->button() == Qt::LeftButton)
+        {
+            drawing = true;
+            startPoint = mapToScene(event->pos());
+            endPoint = startPoint;
+        }
+    }
+
+    void mouseReleaseEvent(QMouseEvent* event) override
+    {
+        if (event->button() == Qt::LeftButton && drawing)
+        {
+            endPoint = mapToScene(event->pos());
+            QGraphicsLineItem* line = new QGraphicsLineItem(QLineF(startPoint, endPoint));
+            line->setPen(QPen(Qt::red));
+            scene->addItem(line);
+            drawing = false;
+        }
+    }
+
+    void mouseMoveEvent(QMouseEvent* event) override
+    {
+        if (drawing)
+        {
+            startPoint = endPoint;
+            endPoint = mapToScene(event->pos());
+            QGraphicsLineItem* tempLine = new QGraphicsLineItem(QLineF(startPoint, endPoint));
+            tempLine->setPen(QPen(Qt::red));
+            scene->addItem(tempLine);
+        }
+    }
+
+private:
+    QGraphicsScene* scene;
+    bool drawing;
+    QPointF startPoint;
+    QPointF endPoint;
+    QPointF lastPos;
+
+    QGraphicsItem* movingItem;
+    QPointF movingOffset;
+};
