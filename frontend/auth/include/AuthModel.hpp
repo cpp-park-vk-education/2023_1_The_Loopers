57caca0cf996dabcdf119975684283e065a2066f
Alexander Zhokhov <a.zhokhov@yandex.ru>
2023-05-30 03:21:35 +0300

    Add network auth communicate

--- frontend/auth/include/AuthModel.hpp
+++ frontend/auth/include/AuthModel.hpp
@@ -1,32 +1,63 @@
 #pragma once
 
-#include "data_container.h"
+#include <websocket_client_session.h>
 
-#include <memory>
+#include <boost/asio.hpp>
+#include <boost/system/error_code.hpp>
+
+#include <chrono>
+#include <mutex>
 #include <string>
+#include <thread>
+
+class AuthDialog;
+class LoginDialog;
+
+namespace
+{
+using namespace std::chrono_literals;
+using namespace inklink::client_connector;
+using error_code = boost::system::error_code;
+} // namespace
 
 namespace inklink::auth
 {
 class AuthModel
 {
-    using DataContainer = inklink::serializer::DataContainer;
-
 public:
-    bool CreateUser(std::string& login, std::string& password);
-    bool Login(std::string& login, std::string& password);
-    void Exit();
-    const std::string GetLogin() const;
-    const std::string GetToken() const;
-
-protected:
-    void SetLogin(const std::string& login);
-    void SetToken(const std::string& token);
-    //    static std::string DinamicSault(int passwordLength);
-    std::string ParseToSend(std::string& login, std::string& password);
-    void ParseToGet(std::string& webSocketData);
+    AuthModel();
+    ~AuthModel();
+    bool createUser(const std::string& login, const std::string& password);
+    bool login(const std::string& login, const std::string& password);
+    void exit();
+    const std::string getLogin() const;
+    const std::string getToken() const;
+    std::string LoginParseToSend(std::string& login, std::string& password);
+    std::string RegParseToSend(std::string login, std::string password);
+    void LoginSend(std::string message);
+    void RegSend(std::string message);
 
 private:
-    std::string m_login;
+    void setLogin(const std::string& login);
+    void setToken(const std::string& token);
+    void LoginParseToGet(std::string& webSocketData);
+    void RegParseToGet(std::string& webSocketData);
+    void SaveTokenEtcForFutureUse(const DataContainer& data);
+
+private:
+    AuthView* m_authview;
+    LoginView* m_loginview;
+
+    std::weak_ptr<IClientSession> m_session;
+
+    boost::asio::io_context m_ioContext;
+
+    boost::asio::any_io_executor m_ioContextExecutor;
+
+    std::thread m_thread_ioContext;
+
+    std::mutex m_tokenMutex;
     std::string m_token;
+    std::string m_login;
 };
 } // namespace inklink::auth
--- frontend/auth/include/AuthView.hpp
+++ frontend/auth/include/AuthView.hpp
@@ -2,28 +2,33 @@
 
 #include "AuthModel.hpp"
 
-#include <QDialog>
 #include <QGraphicsView>
 #include <QLineEdit>
 #include <QString>
 #include <QWidget>
 
+class AuthModel;
+
 namespace inklink::auth
 {
-class AuthDialog : public QDialog
+class AuthDialog : public QWidget
 {
+    Q_OBJECT
 public:
     AuthDialog(QWidget* parent = nullptr);
-
     ~AuthDialog() = default;
 
 private:
     void OnLoginButtonClicked();
-
     void OnCreateButtonClicked();
+    void NotifyGotResultFromNetwork(bool result);
+    void DoOnGotResultFromNetwork(bool result);
+signals:
+    void GotResultFromNetwork(bool result);
 
 private:
     QLineEdit* usernameLine;
     QLineEdit* passwordLine;
+    AuthModel* m_model;
 };
 } // namespace inklink::auth
