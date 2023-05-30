407e04f33aabbb86b292215510340879be00b330
Alexander Novak <sasha_7tdd7@mail.ru>
2023-05-31 02:06:23 +0300

    draw model comment

--- frontend/draw/include/DrawModel.h
+++ frontend/draw/include/DrawModel.h
@@ -1,80 +1,80 @@
-#pragma once
+// #pragma once
 
-#include "IObject.h"
+// #include "IObject.h"
 
-#include <QObject>
+// #include <QObject>
 
-#include <boost/asio.hpp>
-#include <boost/system/error_code.hpp>
+// #include <boost/asio.hpp>
+// #include <boost/system/error_code.hpp>
 
-#include <limits>
-#include <random>
-#include <string>
-#include <thread>
-#include <vector>
+// #include <limits>
+// #include <random>
+// #include <string>
+// #include <thread>
+// #include <vector>
 
-namespace inklink::client_connector
-{
-class IClientSession;
-} // namespace inklink::client_connector
+// namespace inklink::client_connector
+// {
+// class IClientSession;
+// } // namespace inklink::client_connector
 
-namespace inklink::draw
-{
-class DrawView;
-} // namespace inklink::draw
+// namespace inklink::draw
+// {
+// class DrawView;
+// } // namespace inklink::draw
 
-using namespace boost::asio;
+// using namespace boost::asio;
 
-namespace inklink::draw
-{
-class DrawModel : public QObject
-{
-    Q_OBJECT
+// namespace inklink::draw
+// {
+// class DrawModel : public QObject
+// {
+//     Q_OBJECT
 
-public:
-    DrawModel(QObject* parent);
+// public:
+//     DrawModel(QObject* parent);
 
-    ~DrawModel();
+//     ~DrawModel();
 
-    std::string Serialize(int actionType, int figureId, int type);
-    void Send(std::string& message);
-    void SetFilename(std::string& filename);
+//     std::string Serialize(int actionType, int figureId, int type);
+//     void Send(std::string& message);
+//     void SetFilename(std::string& filename);
 
-    // protected:
-    //     void addObject(size_t, size_t, std::vector<Point>&);
-    //     void addObject(size_t, Point&, size_t, size_t);
-    //     void addObject(size_t, std::string&, Point&, size_t);
+//     // protected:
+//     //     void addObject(size_t, size_t, std::vector<Point>&);
+//     //     void addObject(size_t, Point&, size_t, size_t);
+//     //     void addObject(size_t, std::string&, Point&, size_t);
 
-signals:
-    // void NewObject(IObject*); // QObject->setParent(DrawView);
-    // or in model when constructing IObject(DrawView);
+// signals:
+//     // void NewObject(IObject*); // QObject->setParent(DrawView);
+//     // or in model when constructing IObject(DrawView);
 
-    void GotNewMsg(const char* msg);
-    // DoOnRead(const std::string& msg) { emit GotNewMsg(msg.c_str()); }
-    // so that it will be called in "main" thread
+//     void GotNewMsg(const char* msg);
+//     // DoOnRead(const std::string& msg) { emit GotNewMsg(msg.c_str()); }
+//     // so that it will be called in "main" thread
 
-    //
-    // Q_DECLARE_META_TYPE(DataContainer);
-    // qRegisterMetaType(DataContainer); // if you want to use DataContainer in signals/slots
-    //
+//     //
+//     // Q_DECLARE_META_TYPE(DataContainer);
+//     // qRegisterMetaType(DataContainer); // if you want to use DataContainer in signals/slots
+//     //
 
-private:
-    using IClientSession = client_connector::IClientSession;
+// private:
+//     using IClientSession = client_connector::IClientSession;
 
-private:
-    void Deserialize(const std::string& message);
-    [[nodiscard]] int GenerateRandomNumber();
+// private:
+//     void Deserialize(const std::string& message);
+//     [[nodiscard]] int GenerateRandomNumber();
 
-    std::mt19937_64 m_gen;
-    std::uniform_int_distribution<int> m_dis{std::numeric_limits<int>::min(), std::numeric_limits<int>::max()};
+//     std::mt19937_64 m_gen;
+//     std::uniform_int_distribution<int> m_dis{std::numeric_limits<int>::min(), std::numeric_limits<int>::max()};
 
-    DrawView* m_view = nullptr;
-    io_context m_ioContext;
-    any_io_executor m_ioContextExecutor;
-    std::thread m_threadIoContext;
-    std::weak_ptr<IClientSession> m_storageSession;
-    std::weak_ptr<IClientSession> m_accessSession;
-    std::vector<ObjectWithAttributes*> m_objects;
-    std::string m_filename = "new_file";
-};
-} // namespace inklink::draw
+//     DrawView* m_view = nullptr;
+//     io_context m_ioContext;
+//     any_io_executor m_ioContextExecutor;
+//     std::thread m_threadIoContext;
+//     std::weak_ptr<IClientSession> m_storageSession;
+//     std::weak_ptr<IClientSession> m_accessSession;
+//     std::vector<ObjectWithAttributes*> m_objects;
+//     std::string m_filename = "new_file";
+// };
+// } // namespace inklink::draw
