f8a27e63dcd0406db559c23763ec5355d5560f39
Gallaann <reindzer26rus@gmail.com>
2023-05-30 03:05:31 +0300

    add free line item

---
+++ frontend/draw/include/FreeLineItem.h
@@ -0,0 +1,74 @@
+#include <QGraphicsSceneMouseEvent>
+#include <QGraphicsObject>
+#include <QPainter>
+#include <QStyleOptionGraphicsItem>
+#include <QVector>
+
+class FreeLineItem : public QGraphicsObject
+{
+public:
+    FreeLineItem(QGraphicsItem *parent = nullptr)
+            : QGraphicsObject(parent), m_selected(false)
+    {
+        setFlag(ItemIsSelectable);
+        setAcceptHoverEvents(true);
+    }
+
+    QRectF boundingRect() const override
+    {
+        qreal maxX = -std::numeric_limits<qreal>::max();
+        qreal maxY = -std::numeric_limits<qreal>::max();
+        qreal minX = std::numeric_limits<qreal>::max();
+        qreal minY = std::numeric_limits<qreal>::max();
+
+        for (const QPointF& point : m_points) {
+            maxX = std::max(maxX, point.x());
+            maxY = std::max(maxY, point.y());
+            minX = std::min(minX, point.x());
+            minY = std::min(minY, point.y());
+        }
+
+        qreal adjust = 1.0;
+        return QRectF(minX - adjust, minY - adjust, maxX - minX + 2 * adjust, maxY - minY + 2 * adjust);
+    }
+
+    void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget) override
+    {
+        Q_UNUSED(widget);
+
+        painter->setRenderHint(QPainter::Antialiasing);
+
+        if (option->state & QStyle::State_Selected)
+            painter->setPen(Qt::red);
+        else
+            painter->setPen(Qt::black);
+
+        painter->drawPolyline(m_points.data(), m_points.size());
+    }
+
+    void mousePressEvent(QGraphicsSceneMouseEvent *event) override
+    {
+        if (event->button() == Qt::LeftButton)
+            m_selected = true;
+    }
+
+    void mouseMoveEvent(QGraphicsSceneMouseEvent *event) override
+    {
+        if (m_selected)
+        {
+            m_points << mapToParent(event->pos());
+            prepareGeometryChange();
+            update();
+        }
+    }
+
+    void mouseReleaseEvent(QGraphicsSceneMouseEvent *event) override
+    {
+        if (event->button() == Qt::LeftButton)
+            m_selected = false;
+    }
+
+private:
+    QVector<QPointF> m_points;
+    bool m_selected;
+};
