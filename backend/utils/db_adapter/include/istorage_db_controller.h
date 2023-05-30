d780b4e1895146dc29e9596c88eebb40da3039cf
kingjoram <alex.v.danilchenko@yandex.ru>
2023-05-31 01:26:19 +0300

    start integration

--- backend/utils/db_adapter/include/istorage_db_controller.h
+++ backend/utils/db_adapter/include/istorage_db_controller.h
@@ -22,24 +22,14 @@ public:
     [[nodiscard]] virtual std::filesystem::path GetFilePath(const std::string& fileName,
                                                             const std::string& login) const = 0;
 
-<<<<<<< HEAD
     // root - is the file that created session
     [[nodiscard]] virtual std::string GetGraphArcs(const std::string& rootFileName, const std::string& vertexFileName,
                                                    const std::string& login) const = 0;
-=======
-    //root - is the file that created session
-    [[nodiscard]] virtual std::string GetGraphArcs(const std::string& rootFileName, const std::string& vertexFileName,
-                                                  const std::string& login) const = 0;
->>>>>>> 364648ff5405f479f4f2bb4f172023f566ac02b0
 
     [[nodiscard]] virtual std::string GetAllFilesForUser(const std::string& login) const = 0;
 
     virtual void InsertRootFile(const std::string& fileName, const std::string& login,
-<<<<<<< HEAD
                                 const std::filesystem::path& filePath) const = 0;
-=======
-                            const std::filesystem::path& filePath) const = 0;
->>>>>>> 364648ff5405f479f4f2bb4f172023f566ac02b0
 
     virtual void InsertGraphArc(const std::string& login, const std::string& rootFileName,
                                 const std::string& fromFileName, const std::string& toFileName) const = 0;
@@ -52,8 +42,4 @@ public:
 protected:
     std::shared_ptr<StorageDbAdapter> m_adapter;
 };
-<<<<<<< HEAD
-} // namespace inklink::db_controlle
-=======
 } // namespace inklink::db_controller
->>>>>>> 364648ff5405f479f4f2bb4f172023f566ac02b0
